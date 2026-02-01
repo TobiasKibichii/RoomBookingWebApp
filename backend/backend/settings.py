fd66ebf67abd76092a524182c0050f9e3fbc694a
toby77777777 <kibichiitoby4@gmail.com>
2026-02-01 12:55:29 +0300

    Creating, ConfiguringRoom Images and connecting them to Rooms - models, serializers...

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,3 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+#For Serving Offline Images
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,3 +26,14 @@ urlpatterns = [
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls'))
 ]
+
+
+
+# serve media files in development
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
